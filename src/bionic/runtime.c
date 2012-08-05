0133707780cd35215f81bab9e3cdc017af94eabe
Arvid Picciani <aep@exys.org>
2012-08-05 14:53:45 +0200

    bionic includes the compiler runtime, musl does not
    so just wrap the runtime for now until we can do
    this properly in dynlink or something

---
+++ src/bionic/runtime.c
@@ -0,0 +1,9 @@
+long long signed int __divdi3(long long signed int a, long long signed int b)
+{
+    return a/b;
+}
+
+long long signed int __moddi3(long long signed int a, long long signed int b)
+{
+    return a % b;
+}
